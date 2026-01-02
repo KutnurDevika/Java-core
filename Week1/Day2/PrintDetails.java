7bda05ec5eeb16a31c5bea6592e260642c1a90e8
devikakutnur-creator <devikakutnur@gmail.com>
2026-01-02 21:11:26 +0530

    Add Week 1 Day 2 PrintDetails program

---
+++ Week1/Day2/PrintDetails.java
@@ -0,0 +1,10 @@
+class PrintDetails{
+  public static void main(String[] args){
+    String name="Devika Kutnur";
+    int age=18;
+    double marks=80.5;
+    System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+  }
+}
