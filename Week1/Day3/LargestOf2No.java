5c0f3bd548df7b511023ad285f230d287e354711
KutnurDevika <devikakutnur@gmail.com>
2026-01-03 09:25:38 +0530

    Add Week 1 Day 3 largest of 2 No's program

---
+++ Week1/Day3/LargestOf2No.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class LargestOf2No{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a value: ");
+int a=sc.nextInt();
+System.out.println("Enter b value: ");
+int b=sc.nextInt();
+if(a>b){
+System.out.println("a is greater than b");
+}
+else{
+System.out.println("b is greater than a");
+}
+}
+}
