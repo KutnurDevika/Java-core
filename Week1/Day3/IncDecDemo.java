92858f66446d02c61a12e25a3bad29ea7150b66b
KutnurDevika <devikakutnur@gmail.com>
2026-01-03 10:24:35 +0530

    Add Week 1 Day 3 increment and decrement program

---
+++ Week1/Day3/IncDecDemo.java
@@ -0,0 +1,12 @@
+public class IncDecDemo{
+public static void main(String[] args){
+int x=10;
+System.out.println("Initial value of x = " + x);
+System.out.println("x++ = " + (x++));
+ System.out.println("After x++ , x = " + x);
+System.out.println("++x = " + (++x));
+System.out.println("x-- = " + (x--)); 
+System.out.println("After x-- , x = " + x);
+System.out.println("--x = " + (--x));
+}
+}
